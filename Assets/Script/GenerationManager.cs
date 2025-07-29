568904e386eada777b6d96424c16f1d4ad44168d
momoe13 <131223093+momoe13@users.noreply.github.com>
2025-07-29 13:56:12 +0900

    Kキーで敵が出るよう設定

--- Assets/Script/GenerationManager.cs
+++ Assets/Script/GenerationManager.cs
@@ -6,10 +6,16 @@ public class GenerationManager : MonoBehaviour
     bool GeneratFlg= false;
     [SerializeField]//生成アイテム
     GameObject[] CreatePrefabs;
+    float gameTime= 0;
 
     private void Update()
     {
         if (GeneratFlg) PLGeneration();
+        //ボスが出るまで更新する
+        gameTime = Time.time;
+        
+        //敵がｎ体以下なら生成
+        EnemyGeneration();
     }
 
     private void PLGeneration()
@@ -17,7 +23,16 @@ public class GenerationManager : MonoBehaviour
         Instantiate(CreatePrefabs[Type], this.transform.position, Quaternion.identity);
         GeneratFlg = false;
     }
-    public void SetNum(int num)
+    private void EnemyGeneration()
+    {
+        //TODO:01 消す
+        if (Input.GetKeyDown(KeyCode.K)) {
+            Instantiate(CreatePrefabs[0], new Vector2(this.transform.position.x+18.0f, this.transform.position.y), Quaternion.identity);
+        }
+    }
+
+
+        public void SetNum(int num)
     {
         Type = num;
         GeneratFlg = true;
