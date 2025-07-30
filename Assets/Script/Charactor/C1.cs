9fe7a01cc2070b523a19916247b9184e0e7843b6
momoe13 <131223093+momoe13@users.noreply.github.com>
2025-07-30 17:41:30 +0900

    キャラの攻撃判定修正
    当たり判定を四角に統一し、大きさもそろえよう

--- Assets/Script/Charactor/C1.cs
+++ Assets/Script/Charactor/C1.cs
@@ -3,24 +3,24 @@ using static UnityEngine.GraphicsBuffer;
 
 public class C1: Character_Controller
 {
-    [SerializeField] int attackPower;
-    bool targetFlg = true;
-    Character_Controller controller;
+    //[SerializeField] int attackPower;
+    //bool targetFlg = true;
+    //Character_Controller controller;
 
-    protected override void Attack()
-    {
-        Debug.Log("攻撃！");
-        //ターゲット名からターゲットとそのスクリプトを取得
-        if(targetFlg)
-        {
-            controller = GameObject.Find(target).GetComponent<Character_Controller>();
-            targetFlg = false;
-        }
+    //protected override void Attack()
+    //{
+    //    Debug.Log("攻撃！");
+    //    //ターゲット名からターゲットとそのスクリプトを取得
+    //    if(targetFlg)
+    //    {
+    //        controller = GameObject.Find(target).GetComponent<Character_Controller>();
+    //        targetFlg = false;
+    //    }
 
-        //Damege関数呼び出し
-        controller.Damage(attackPower);
+    //    //Damege関数呼び出し
+    //    controller.Damage(attackPower);
 
-    }
+    //}
 
   
 }
--- Assets/Script/Charactor/Character_Controller.cs
+++ Assets/Script/Charactor/Character_Controller.cs
@@ -1,5 +1,6 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.XR;
 using UnityEngine.UI;
 
 public class Character_Controller : MonoBehaviour
@@ -12,11 +13,16 @@ public class Character_Controller : MonoBehaviour
     [SerializeField] float      speed;//�ړ����x
     [SerializeField] float          hp;//�̗�
 
-    [SerializeField] float attack_Timing;//攻撃タイミング
+    [SerializeField] float attack_Timing;//クールタイム
 
     [SerializeField]
     float                   attack_Time;//攻撃までのカウントダウン用タイム
+
+    [SerializeField]
     protected string            target;//攻撃するオブジェクト
+    bool targetFlg = true;
+
+    [SerializeField] int attackPower;
 
     [Header ("ダメージUI用Prefab")]
     [SerializeField] GameObject DamagePre;
@@ -25,8 +31,12 @@ public class Character_Controller : MonoBehaviour
     [SerializeField]
     protected bool MoveFlg = false;
 
+    Character_Controller controller;
+
     Vector2 pos, scale;//死亡モーション用。
     bool DieFlg = false;//死亡判定
+
+    CircleCollider2D circleCollider;
 
     [Header("ドロップアイテム")]
     [SerializeField]
@@ -42,6 +52,7 @@ public class Character_Controller : MonoBehaviour
 
         scale= transform.localScale;
         DamageCanvas = GameObject.Find("DamageCanvas").GetComponent<Canvas>();
+        circleCollider = GetComponent<CircleCollider2D>();
     }
 
     private void Update()
@@ -51,7 +62,7 @@ public class Character_Controller : MonoBehaviour
         else
         {
             Move();
-            attack_Time += Time.time;
+            attack_Time += Time.deltaTime;
 
         }
 
@@ -68,10 +79,23 @@ public class Character_Controller : MonoBehaviour
 
     protected virtual void Attack()
     {
-       if(attack_Time < attack_Timing)return;
-        attack_Time = 0;
-        
+        Debug.Log(string.Format( "{0}/{1}", attack_Time,attack_Timing));
+        if (attack_Time < attack_Timing){ return; }
+
+        //ターゲット名からターゲットとそのスクリプトを取得
+        if (targetFlg)
+        {
+            controller = GameObject.Find(target).GetComponent<Character_Controller>();
+            targetFlg = false;
+        }
+        attack_Time = 0.0f;
+        Debug.Log("攻撃！");
+        //Damege関数呼び出し
+        controller.Damage(attackPower);
+       
+
     }
+
     public void Damage(int dm)
     {
         if (DieFlg) return ;
@@ -118,13 +142,15 @@ public class Character_Controller : MonoBehaviour
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        MoveFlg = true;
+        if( Vector3.Distance(transform.position,collision.transform.position) <= circleCollider.radius * 0.9f) MoveFlg = true;
         target = collision.gameObject.name;
         Attack();
+        Debug.Log("Wait");
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         MoveFlg = false;
         target = null;
+        Debug.Log("ばいばい");
     }
 }
