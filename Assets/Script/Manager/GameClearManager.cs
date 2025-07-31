db6fbab0d9ebb6cda93105f98285496b8d0c68cb
Fuyu <fuyu2363@gmail.com>
2025-08-01 04:27:06 +0900

    ゲームクリア画面でクリックしたらタイトル画面へ

---
+++ Assets/Script/Manager/GameClearManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GameClearManager : MonoBehaviour
+{
+    private void Update()
+    {
+        //クリックされたらタイトルに戻る
+        if (Input.GetMouseButton(0))
+        {
+            SceneChange.TitleLordScene();
+        }
+    }
+}
