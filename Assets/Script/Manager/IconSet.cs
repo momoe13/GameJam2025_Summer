7a4f99e7b407d7f1f1f9e63a6a7356926a23f2da
shin <shin@vicizube.net>
2025-08-01 00:48:35 +0900

    Update StageSelect

--- Assets/Script/Manager/IconSet.cs
+++ Assets/Script/Manager/IconSet.cs
@@ -1,10 +1,20 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class IconSet : MonoBehaviour
 {
+    private StageSelect _stageSelect;
+
+    [SerializeField]
+    private int stageNum;
+    private void Start()
+    {
+        _stageSelect = GetComponentInParent<StageSelect>();
+    }
+
     private void OnMouseDown()
     {
-        transform.GetChild(0).gameObject.SetActive(true);  
+        _stageSelect.OnStageButtonSelected.Invoke(stageNum);  
     }
 }
--- Assets/Script/Manager/StageSelect.cs
+++ Assets/Script/Manager/StageSelect.cs
@@ -1,16 +1,32 @@
+using System;
 using UnityEngine;
 
 public class StageSelect : MonoBehaviour
 {
+    [SerializeField] GameObject[] StageObj;
+
+    public Action<int> OnStageButtonSelected;
     [SerializeField]
-    GameObject[] StageObj;
+    private int currentStage;
 
     private void Start()
     {
-        for (int i = 0; i < StageObj.Length; i++)
-        {
-            StageObj[i].SetActive(false);
-        }
+        SelectStage(currentStage);
+        OnStageButtonSelected += SelectStage;
     }
 
+    private void SelectStage(int stage)
+    {
+        for (int i = 0; i < StageObj.Length; i++)
+        {
+            StageObj[i].SetActive(i==stage);
+        }
+        currentStage = stage + 2; // Adjust with Scene Number
+    }
+
+    public void StageStart()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(currentStage);
+        Time.timeScale = 1.0f;
+    }
 }
