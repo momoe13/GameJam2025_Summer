c08ea80c94927dfd5dba0b4338876cf89de37606
c4316 <c4316@oic.jp>
2025-07-31 14:55:18 +0900

    敵の自動生成
    敵が減ると新しく追加されるようにしました

--- Assets/Script/Manager/GenerationManager.cs
+++ Assets/Script/Manager/GenerationManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GenerationManager : MonoBehaviour
@@ -6,29 +7,48 @@ public class GenerationManager : MonoBehaviour
     bool GeneratFlg= false;
     [SerializeField]//生成アイテム
     GameObject[] CreatePrefabs;
+
+    int maxEne=3;
     float gameTime= 0;
+
+    //敵キャラの生成数を保存する
+    [SerializeField]
+    List<GameObject> EnemyBox;
+    int eneType = 0;
+
 
     private void Update()
     {
         if (GeneratFlg) PLGeneration();
         //ボスが出るまで更新する
-        gameTime = Time.time;
+       // gameTime = Time.time;
         
+        for (int i = 0; i < EnemyBox.Count; i++)
+        {
+            if(EnemyBox[i] == null) EnemyBox.Remove(EnemyBox[i]);
+        }
+
         //敵がｎ体以下なら生成
-        EnemyGeneration();
+        if(EnemyBox.Count<maxEne)
+        {
+
+            EnemyGeneration();
+        }
     }
 
     private void PLGeneration()
     {
         Instantiate(CreatePrefabs[Type], this.transform.position, Quaternion.identity);
         GeneratFlg = false;
+
     }
     private void EnemyGeneration()
     {
-        //TODO:01 消す
-        if (Input.GetKeyDown(KeyCode.K)) {
-            Instantiate(CreatePrefabs[0], new Vector2(this.transform.position.x+18.0f, this.transform.position.y), Quaternion.identity);
-        }
+        GameObject ene= Instantiate(CreatePrefabs[eneType], new Vector2(this.transform.position.x + 18.0f, this.transform.position.y), Quaternion.identity);
+       
+        EnemyBox.Add(ene);
+        eneType++;
+        if (eneType > 1) eneType = 0;
     }
 
 
