04ae7b2f6307818ee0efcf936d13dd0c1b6967f2
momoe13 <131223093+momoe13@users.noreply.github.com>
2025-07-31 12:58:46 +0900

    ボスが死んだらクリア処理を追加。デバッグ用にESCキーを押すと５ダメージ入るよう設計

--- Assets/Script/Charactor/Boss.cs
+++ Assets/Script/Charactor/Boss.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Boss:Character_Controller
 {
+    [SerializeField] GameObject Clar;
     protected override void Die()
     {
         base.Die();
@@ -9,7 +11,13 @@ public class Boss:Character_Controller
         //死んだ
         Debug.Log("まだ動いてるよ");
         //クリアテキスト表示
-     
+        Clar.SetActive(true);
+        Time.timeScale = 0.0f;
         //タイトルに遷移
+        if(Input.GetMouseButtonDown(0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("0_TitleScene");
+            Time.timeScale = 1.0f;
+        }
     }
 }
